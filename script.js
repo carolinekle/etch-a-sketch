48069478aaca22f0e9a6e96ee56cfc89dc567a4c
carolinekle <caroline.klewinowski@gmail.com>
2023-02-09 20:32:36 -0500

    Got the grid, working on adding event listners to dynamic elements

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-let grid = document.getElementById("grid");
+const grid = document.querySelector("#grid");
+const inner = document.querySelectorAll("#inner");
 
 
-    for(i = 0; i < 250; i++){
-    let pixel = document.createElement('pixel');
-    pixel.classList = 'pixel';
-    grid.appendChild(pixel);
-    console.log(pixel);
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+      let cell = document.createElement("div");
+     
+      grid.appendChild(cell).className = "pixel";
+      setListener(pixel);
+    };
+  };
+  
+  makeRows(16, 16);
+
+    grid.addEventListener("click", function(e) {
+
+    if(e.target.contains('pixel')){
+ 
+    alert("clicked");
     }
+   });
