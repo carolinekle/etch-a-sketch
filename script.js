851d33a1379fc1b18b531d10f2617c1d13b049ef
carolinekle <caroline.klewinowski@gmail.com>
2023-02-21 20:46:26 -0500

    Working on changing grid size

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
 const grid = document.querySelector("#grid");
 const inner = document.querySelectorAll("#inner");
+const shake = document.querySelector("shake");
+const right = document.querySelector("right-controller");
+const left = document.querySelector("left-controller");
 
 
-function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
+
+function makeRows(size) {
+    container.style.setProperty('--grid-rows', size);
+    container.style.setProperty('--grid-cols', size);
+    for (c = 0; c < (size * size); c++) {
       let cell = document.createElement("div");
-     
+      cell.addEventListener('mouseover',() => {cell.style.backgroundColor = "black";})
+      //('draggable', 'false')
       grid.appendChild(cell).className = "pixel";
-      setListener(pixel);
+  
     };
   };
   
-  makeRows(16, 16);
+  makeRows(16);
 
-    grid.addEventListener("click", function(e) {
+function changeSize(input){
+  input.addEventListener('keydown', (e) => {
+      if (13 === e.keyCode) {
+        makeRows(input);
+      } 
+    
+    })
+  };
+console.log(changeSize(input));
 
-    if(e.target.contains('pixel')){
- 
-    alert("clicked");
-    }
-   });
+function changeColor(right, left){
+  if(right){
+    cell.style.backgroundColor = {};
+    //rba number must be between 3-252
+  }
+  if(left){
+
+  }
+}
+
+right.forEach(right =>{
+  right.addEventListener('click', function(){
+      changeColor();
+  })
+})
+
+left.forEach(left =>{
+  left.addEventListener('click', function(){
+      changeColor();
+  })
+})
