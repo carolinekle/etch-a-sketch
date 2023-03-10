4dcaa6e01e9ff33ea01cf5558307110be4b58c96
carolinekle <caroline.klewinowski@gmail.com>
2023-03-09 21:01:24 -0500

    get most functionality across. changeColor and styles needs some tweaks

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const inner = document.querySelectorAll("#inner");
 const shake = document.querySelector(".shake");
 const right = document.querySelector(".right-controller");
 const left = document.querySelector(".left-controller");
- const change = document.querySelector(".change"); 
+const change = document.querySelector(".change"); 
+
+let color = "black";
 
 
 
@@ -12,7 +14,7 @@ function makeRows(size) {
     container.style.setProperty('--grid-cols', size);
     for (c = 0; c < (size * size); c++) {
       let cell = document.createElement("div");
-      cell.addEventListener('mouseover',() => {cell.style.backgroundColor = "black";})
+      cell.addEventListener('mouseover',() => {cell.style.backgroundColor = color;})
       //('draggable', 'false')
       grid.appendChild(cell).className = "pixel";
   
@@ -21,35 +23,11 @@ function makeRows(size) {
   
 makeRows(16)
 
-function changeColor(){
-  var num = Math.round(0xffffff * Math.random());
-  var r = num >> 16;
-  var g = num >> 8 & 255;
-  var b = num & 255;
-  return 'rgb(' + r + ', ' + g + ', ' + b + ')';
-};
 
 console.log(changeColor);
 
- change.addEventListener('click', () => {
-      
-        let newSize = document.querySelector(".new-size").innerHTML;
-        if(newSize>80){
-          window.alert("Try a smaller number!");//not working
-        }
-      
-        else{
-        makeRows(newSize);
-        let currentSize = document.querySelector("current-size");
-        currentSize.appendChild(newSize);
-        }
-//else if for when the input is deleted
-      } 
-    
-    ); 
-console.log(changeSize(input));
 
-/* right.addEventListener('click', () => {
+right.addEventListener('onclick', () => {
     cell.style.backgroundColor = {
       Random(){
         var num = Math.round(0xffffff * Math.random());
@@ -61,18 +39,41 @@ console.log(changeSize(input));
     };
     //rba number must be between 3-252
   }
+); 
 
-}); */
 
-right.forEach(right =>{
-  right.addEventListener('click', function(){
-      changeColor();
-  })
-})
 
-left.forEach(left =>{
-  left.addEventListener('click', function(){
-      changeColor();
-  })
-})
+function changeColor(){
+  var num = Math.round(0xffffff * Math.random());
+  var r = num >> 16;
+  var g = num >> 8 & 255;
+  var b = num & 255;
+  return color ='rgb(' + r + ', ' + g + ', ' + b + ')';
+};
 
+shake.addEventListener('click',() => {{
+  let pixels = document.querySelectorAll(".pixel");
+  pixels.forEach((cell) => cell.style.backgroundColor = "white");
+}});
+
+function revertColor(){
+  return color = "black";
+}
+
+ /* change.addEventListener('click', () => {
+      
+        let newSize = document.querySelector(".new-size").innerHTML;
+        if(newSize<8 || newSize>80){
+        makeRows(newSize);
+        let currentSize = document.querySelector("current-size");
+        currentSize.appendChild(newSize);
+        }
+      
+        else{
+          makeRows(16)
+          console.log("Try a smaller number!");//not working
+        }
+//else if for when the input is deleted
+      } 
+    
+    );  */
